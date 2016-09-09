aad047a8010e3962fb491bdfac2a603f828a217d
Aldrin Lenny <aldrin@isletsystems.com>
2016-09-09 19:08:10 +1200

    code tweaks to support XCode8  GM release and Swift3

--- SCLAlertViewTests/SCLAlertViewTests.swift
+++ SCLAlertViewTests/SCLAlertViewTests.swift
@@ -28,7 +28,7 @@ class SCLAlertViewTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
